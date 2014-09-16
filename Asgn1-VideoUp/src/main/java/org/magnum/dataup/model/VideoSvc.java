e51bec708f5d7bc066c017fed2631298ff0e6c41
Tommy Thomas <tommyt@uchicago.edu>
2014-09-16 16:15:40 -0500

    Adding asgn 1.

---
+++ Asgn1-VideoUp/src/main/java/org/magnum/dataup/model/VideoSvc.java
@@ -0,0 +1,89 @@
+/*
+ * 
+ * Copyright 2014 Jules White
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ */
+package org.magnum.dataup.model;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicLong;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+@Controller
+public class VideoSvc {
+
+	private static final String VIDEO_SVC_PATH = "/video";
+	private static final AtomicLong currentId = new AtomicLong(0L);
+	private Map<Long, Video> videos = new HashMap<Long, Video>();
+
+	@RequestMapping(value = VIDEO_SVC_PATH, method = RequestMethod.GET)
+	public @ResponseBody
+	Collection<Video> getVideoList() {
+		return videos.values();
+	}
+
+	@RequestMapping(value = VIDEO_SVC_PATH, method = RequestMethod.POST)
+	public @ResponseBody
+	Video addVideo(@RequestBody Video v) {
+		this.save(v);
+		v.setDataUrl(this.getDataUrl(v.getId()));
+		return v;
+	}
+
+	public Video save(Video entity) {
+
+		checkAndSetId(entity);
+
+		videos.put(entity.getId(), entity);
+
+		return entity;
+
+	}
+
+	private void checkAndSetId(Video entity) {
+
+		if (entity.getId() == 0) {
+
+			entity.setId(currentId.incrementAndGet());
+
+		}
+
+	}
+	
+	 private String getDataUrl(long videoId){
+         String url = getUrlBaseForLocalServer() + "/video/" + videoId + "/data";
+         return url;
+     }
+
+  	private String getUrlBaseForLocalServer() {
+		   HttpServletRequest request = 
+		       ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest();
+		   String base = 
+		      "http://"+request.getServerName() 
+		      + ((request.getServerPort() != 80) ? ":"+request.getServerPort() : "");
+		   return base;
+		}
+}
